c1645b2bfad5dc3b282867e751de0c252fd48e6e
unknown <ralmauhay@yahoo.com>
2026-07-30 16:51:33 -0700

    Synced changes between two workplaces

--- scripts/temp_models.py
+++
@@ -1,9 +0,0 @@
-# DELETE THIS
-
-from wipe_folder import wipe_folder
-from temperature_forecaster.fourier_training import train_and_store_models
-from temperature_forecaster.paths import ROOT
-from temperature_forecaster.__init__ import weather_station_coords
-
-for city in weather_station_coords.keys():
-    print()
